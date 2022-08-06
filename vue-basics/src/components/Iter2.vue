837f197110ad1227f92dd03370cf2adb2d4d3dcd
Macia Estela <maciaestela97@gmail.com>
2022-08-06 10:56:11 +0200

    teration 3

--- vue-basics/src/components/Iter2.vue
+++ vue-basics/src/components/Iter2.vue
@@ -2,17 +2,28 @@
     <div>
         <h1>Iteration 2. Sum result = {{ 2 + 2 }}</h1>
         <h2>{{onCompute}}</h2>
+        <div>
+            <button @click="toggleSpan">Toggle span</button>
+            <span v-if="showSpan">I'm a SPAN</span>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
   data() {
-
+    return{
+        showSpan: true
+    }
   },
   computed:{
     onCompute(){
         return "From onCompute() function";
+    }
+  },
+  methods:{
+    toggleSpan(){
+        this.showSpan = !this.showSpan;
     }
   }
 }
