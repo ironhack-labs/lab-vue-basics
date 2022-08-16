4164907f5a2f5d64e33c757024385c30c3496b61
Ari Tomas Bussi <abussy@gmail.com>
2022-08-16 20:40:47 +0200

    iteration 4

--- lab-basics/src/App.vue
+++ lab-basics/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <Navbar />
   <Footer />
+  <PostList />
 </template>
 
 <script>
 import Navbar from './components/TheNavbar.vue';
 import Footer from './components/TheFooter.vue';
+import PostList from './components/PostList.vue';
 
 export default {
   name: 'App',
   components: {
     Navbar,
-    Footer
+    Footer,
+    PostList
 }
 }
 </script>
---
+++ lab-basics/src/components/PostList.vue
@@ -0,0 +1,27 @@
+<script setup>
+const posts = [
+    {
+        title: 'Títutlo 1',
+        description: 'Descripción 1',
+        content: 'Contenido 1'
+    },
+    {
+        title: 'Títutlo 2',
+        description: 'Descripción 2',
+        content: 'Contenido 2'
+    },
+    {
+        title: 'Títutlo 3',
+        description: 'Descripción 3',
+        content: 'Contenido 3'
+    },];
+</script>
+
+<template>
+    <h2>All my posts</h2>
+    <div v-for="post in posts" :key="post.title">
+        <h3>{{post.title}}</h3>
+        <p>{{post.description}}</p>
+        <p>{{post.content}}</p>
+    </div>
+</template>
