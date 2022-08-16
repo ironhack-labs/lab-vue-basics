21673e9f3fd25f9ecddd2cc0123a0749f947279a
andayanda <carmelaespinola@gmail.com>
2022-08-16 21:55:46 +0200

    done

---
+++ segundo/src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <NavBar />
+    <router-view />
+    <MyFooter />
+  </div>
+</template>
+
+<script>
+import NavBar from "@/components/NavBar.vue";
+import MyFooter from "@/components/MyFooter.vue";
+
+export default {
+  name: "App",
+  components: {
+    NavBar,
+    MyFooter,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
