0a83497f44ee39fa80754493fa27b73c73b1261c
Txell <meritxell@gold.barcelona>
2022-08-16 19:40:36 +0200

    iteration 1

--- vue-basics/src/views/AboutView.vue
+++ vue-basics/src/views/AboutView.vue
@@ -1,5 +1,20 @@
 <template>
   <div class="about">
+    <Navbar/>
     <h1>This is an about page</h1>
+    <Footer/>
   </div>
 </template>
+
+<script>
+import Navbar from '../components/Navbar-comp.vue';
+import Footer from '../components/Footer-comp.vue';
+
+export default {
+  name: 'AboutView',
+  components: {
+    Navbar,
+    Footer,
+  },
+};
+</script>
--- vue-basics/src/views/HomeView.vue
+++ vue-basics/src/views/HomeView.vue
@@ -1,18 +1,24 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
+    <Navbar/>
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue';
+import Navbar from '../components/Navbar-comp.vue';
+import Footer from '../components/Footer-comp.vue';
 
 export default {
   name: 'HomeView',
   components: {
     HelloWorld,
+    Navbar,
+    Footer,
   },
 };
 </script>
