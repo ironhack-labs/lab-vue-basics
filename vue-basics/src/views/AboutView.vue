8ee37914450e6eda6263c0821355404b45da17f1
Maria Cassandra <maria.cassandra@gmail.com>
2022-08-18 20:33:18 +0200

    done

--- vue-basics/src/views/AboutView.vue
+++ vue-basics/src/views/AboutView.vue
@@ -5,17 +5,17 @@
     <section class="ejercicio3">
       <h2>Iteration 3</h2>
       <p>My favorite food is: </p>
-      <p v-if="faveFood === true"> I love japanese food! </p>
+      <p v-if="faveFood === true"> I love japanese food! Bring me the sushi! </p>
       <p v-show="faveFood === false"> I'm not so into food - I live on water and air </p>
     </section>
     <section class="ejercicio4">
       <h2>Iteration 4</h2>
       <p> This content is created inside the data  </p>
-        <div v-for="project in projects"
+        <div class="projectCard" v-for="project in projects"
         :key="project.id">
-        <h3>{{ projects.title }}</h3>
-        <h4>{{ projects.description }}</h4>
-        <h5>{{ projects.content }}</h5>
+        <h3>{{ project.title }}</h3>
+        <h4>{{ project.description }}</h4>
+        <h5>{{ project.content }}</h5>
         </div>
     </section>
   </div>
@@ -30,20 +30,20 @@ export default {
       projects: [
         {
           id: 0,
-          title: 'How to do lists in Vue',
-          description: 'Jane Doe',
+          title: 'How to do lists in Vue?',
+          description: 'Written by Jane Doe',
           content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.',
         },
         {
           id: 1,
           title: 'Using v-for with Objects',
-          description: 'John Doe',
+          description: 'Written by John Doe',
           content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.',
         },
         {
           id: 2,
           title: 'How to do lists in Vue and not to go crazy in the process',
-          description: 'Maria Cassandra',
+          description: 'Written by Maria Cassandra',
           content: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, quidem.',
         },
       ],
@@ -51,3 +51,32 @@ export default {
   },
 };
 </script>
+
+<style>
+  .projectCard {
+    display: flex;
+    flex-direction: column;
+    text-align: center;
+  }
+
+  h2 {
+    text-decoration: underline;
+    padding-top: 2rem;
+  }
+
+  h3 {
+    color: #42b983;
+    padding: 0;
+  }
+
+  h4 {
+    padding: 0;
+    margin: 0;
+  }
+
+  h5 {
+    font-style: oblique;
+    padding: 0;
+    margin: 0;
+  }
+</style>
