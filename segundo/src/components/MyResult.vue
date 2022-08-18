e68d493784a9130c8adc379d2d6a65e46f2c5d1a
andayanda <carmelaespinola@gmail.com>
2022-08-18 21:57:52 +0200

    iteration 2 + 3

---
+++ segundo/src/components/MyResult.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h1 :class="{ red: isRed }" @click="toggleRed">
+      Clica y cambiare de color
+    </h1>
+    <p>El resultado de 2+2 es {{ 2 + 2 }}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: "MyResult",
+  data() {
+    return {
+      isRed: false,
+    };
+  },
+  methods: {
+    toggleRed() {
+      this.isRed = !this.isRed;
+    },
+  },
+};
+</script>
+<style>
+.red {
+  color: red;
+}
+p {
+  color: blue;
+}
+</style>
