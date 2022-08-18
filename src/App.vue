b90fcead59dfe3a25f591a357df5ac5c6b76ba98
EduardBru <ebrudomingo@gmail.com>
2022-08-18 15:38:37 +0200

    new branch

---
+++ src/App.vue
@@ -0,0 +1,41 @@
+<template>
+  <NavBar/>
+  <router-view/>
+  <FooterBar/>
+</template>
+
+<style>
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #3b3c3d;
+}
+
+nav {
+  padding: 30px;
+}
+
+nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+nav a.router-link-exact-active {
+  color: #42b983;
+}
+</style>
+<script>
+// @ is an alias to /src
+import NavBar from '@/components/NavBar.vue';
+import FooterBar from '@/components/Footer.vue';
+
+export default {
+  components: {
+    NavBar,
+    FooterBar,
+  },
+};
+</script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,10 @@
+<template>
+  <footer>
+    <h1>This is a footer</h1>
+  </footer>
+</template>
+<script>
+export default {
+  name: 'FooterBar', // ejercicio 1
+};
+</script>
