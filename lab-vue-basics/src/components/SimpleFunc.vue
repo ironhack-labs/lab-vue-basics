9798c68c0b17ca60df915e384474bd094c1d81d1
Irene <ruizdacal@gmail.com>
2022-08-26 18:49:45 +0200

    iteration 2

---
+++ lab-vue-basics/src/components/SimpleFunc.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>
+    {{ msg }}
+  </h1>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'What a nice day to code!',
+    };
+  },
+};
+</script>
+
+<style>
+</style>
