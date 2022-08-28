41d3196eb1eec88ee1a49f6e241958129ce3ab97
Irina <irinakhr@mail.ru>
2022-08-28 17:35:30 +0200

    done

---
+++ myvuebasics/src/components/BeachestoVisit.vue
@@ -0,0 +1,44 @@
+<template>
+  <ul  class ="beachlist" v-for="beach in beaches" :key="beach.title">
+    <li :class ="beachColor">{{
+      beach.title
+    }}</li>
+    <li>{{beach.description}}</li>
+    <li>{{beach.content}}</li>
+  </ul>
+  <button @click='colorchange'>Change the title color</button>
+</template>
+
+<script>
+export default {
+  name: 'BeachestoVisit',
+  data() {
+    return {
+      beaches: [
+        { title: 'Barra:', description: 'Nice beach in Portugal', content: '(Many km of white sand)' },
+        { title: 'Gerra:', description: 'Beatiful beach in Cantabria', content: '(A lot of parking spaces around)' },
+        {
+          title: 'Ribadesella:',
+          description: 'Famous town village in Asturias',
+          content: '(Very touristic)',
+        },
+      ],
+      beachColor: 'titlecolor',
+    };
+  },
+  methods: {
+    colorchange() {
+      this.beachColor = 'titlecolordos';
+    },
+  },
+};
+</script>
+
+<style>
+.beachlist {
+  list-style: none;
+}
+.titlecolordos {
+  color:blue;
+}
+</style>
---
+++ myvuebasics/src/components/MyFooter.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+    <p>{{ 2 + 2}}</p>
+  </div>
+</template>
+<script>
+export default {
+  name: 'MyFooter',
+  data() {
+    return {
+
+    };
+  },
+};
+</script>
