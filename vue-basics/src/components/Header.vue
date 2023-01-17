e6cb486086aad9e8f9c00089dceb2881ca0d413f
lcelaya1 <lauracelayabarenys@gmail.com>
2023-01-17 22:48:05 +0100

    vue basics

---
+++ vue-basics/src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+<Nav />
+</template>
+
+<script setup>
+import Nav from './Nav.vue';
+</script>
+
+<style scoped>
+nav {
+    justify-content: space-evenly;
+    background-color: royalblue;
+}
+</style>
